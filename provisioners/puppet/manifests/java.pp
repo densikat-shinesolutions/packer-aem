7ea040afc53dabc60207fe81e2d9811a1213378b
David Ensikat <david.ensikat@shinesolutions.com>
2017-05-01 09:39:05 +1000

    Testing openjdk removal

--- provisioners/puppet/manifests/java.pp
+++ provisioners/puppet/manifests/java.pp
@@ -15,13 +15,23 @@ class java (
     add_alternative => true,
   }
 
-  each( [6,7,8] ) | $version | {
-    package { ["openjdk-${version}-jdk", "openjdk-${version}-jre"]:
-      ensure => 'purged',
+
+  #each( [6,7,8] ) | $version | {
+  #  package { ["openjdk-${version}-jdk", "openjdk-${version}-jre"]:
+  #    ensure => 'purged',
+  #}
+
+  package { 'openjdk-8-jdk':
+    ensure => 'purged',
+  }
+  
+
+  package { 'openjdk-8-jre':
+    ensure => 'purged',
   }
 
   file { '/etc/ld.so.conf.d/99-libjvm.conf':
-    ensure  => present,
+    ensure  => file,
     content => "/usr/java/latest/jre/lib/amd64/server\n",
     notify  => Exec['/sbin/ldconfig'],
   }
@@ -85,7 +95,7 @@ class java (
     }
 
     file { $collectd_jmx_types_path:
-      ensure  => present,
+      ensure  => file,
       content => file('config/collectd_jmx_types.db'),
       require => Package[$::collectd::install::package_name],
     }
