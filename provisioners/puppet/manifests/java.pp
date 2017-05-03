e7e89082fe725f397c035a2fb9813e6ca9e4264c
David Ensikat <david.ensikat@shinesolutions.com>
2017-05-03 17:11:56 +1000

    Updated Makefile to allow parallel tasks in packer

--- provisioners/puppet/manifests/java.pp
+++ provisioners/puppet/manifests/java.pp
@@ -1,8 +1,8 @@
 class java (
   $tmp_dir,
   $aem_cert_source,
-  $jdkversion = hiera("jdk_oracle::version"),
-  $jdkversion_update = hiera("jdk_oracle::version_update"),
+  $jdkversion = hiera('jdk_oracle::version'),
+  $jdkversion_update = hiera('jdk_oracle::version_update'),
   $install_collectd = true,
   $java_path = '/usr/java/jdk1.8.0_121/bin/java',
   $collectd_cloudwatch_source_url = 'https://github.com/awslabs/collectd-cloudwatch/archive/master.tar.gz',
@@ -16,7 +16,7 @@ class java (
     version         => '8u121',
     type            => 'jdk',
     add_alternative => true,
-  } 
+  }
 
   file { '/etc/ld.so.conf.d/99-libjvm.conf':
     ensure  => file,
@@ -27,11 +27,11 @@ class java (
   exec { '/sbin/ldconfig':
     refreshonly => true,
   }
-  
+
   exec { "alternatives --set java /usr/java/jdk1.${jdkversion}.0_${jdkversion_update}/bin/java":
     path => ['/usr/bin', '/usr/sbin'],
-  } 
-  
+  }
+
   archive { "${tmp_dir}/aem.cert":
     ensure => present,
     source => $aem_cert_source,
